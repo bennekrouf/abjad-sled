cec6e57453da3ad5ed32fa717228468a78a742db
Bennekrouf <mohamed.bennekrouf@gmail.com>
2024-01-01 13:13:38 +0100

    Add logs

--- src/api/audio_files.rs
+++ src/api/audio_files.rs
@@ -1,18 +1,23 @@
 use rocket::get;
 use rocket::fs::NamedFile;
 use std::path::PathBuf;
-use log::info;
+use log::{info, error};
 
 #[get("/files/<file..>")]
 pub async fn audio_files(file: PathBuf) -> Option<NamedFile> {
-    let full_path = PathBuf::from("data").join(file);
+    let full_path = PathBuf::from("data").join(&file);
     
-     // Log the directory being accessed
-     if let Some(parent) = full_path.parent() {
-        info!("Accessing file in directory: {}", parent.display());
-    } else {
-        info!("No parent directory found for path: {}", full_path.display());
-    }
+    // Log the full file path being accessed
+    info!("Attempting to access file at path: {}", full_path.display());
 
-    NamedFile::open(full_path).await.ok()
+    match NamedFile::open(&full_path).await {
+        Ok(file) => {
+            info!("Successfully opened file: {}", full_path.display());
+            Some(file)
+        },
+        Err(e) => {
+            error!("Error opening file: {}. Error: {}", full_path.display(), e);
+            None
+        }
+    }
 }
