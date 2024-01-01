9dd60acbc1c380008f252852c2227b70b026d0a2
Bennekrouf <mohamed.bennekrouf@gmail.com>
2024-01-01 13:21:01 +0100

    Fix to access data folder

--- src/api/audio_files.rs
+++ src/api/audio_files.rs
@@ -2,21 +2,20 @@ use rocket::get;
 use rocket::fs::NamedFile;
 use std::path::PathBuf;
 use log::{info, error};
+use crate::utils::yml_path::get_data_folder_path;
 
 #[get("/files/<file..>")]
 pub async fn audio_files(file: PathBuf) -> Option<NamedFile> {
-    let full_path = PathBuf::from("data").join(&file);
-    
-    // Log the full file path being accessed
+    let base_path = get_data_folder_path(); // Get the base data directory
+    let full_path = base_path.join(file); // Join with the requested file path
+
+    // Log the attempted access path
     info!("Attempting to access file at path: {}", full_path.display());
 
-    match NamedFile::open(&full_path).await {
-        Ok(file) => {
-            info!("Successfully opened file: {}", full_path.display());
-            Some(file)
-        },
+    match NamedFile::open(full_path).await {
+        Ok(file) => Some(file),
         Err(e) => {
-            error!("Error opening file: {}. Error: {}", full_path.display(), e);
+            error!("Error opening file: {}", e);
             None
         }
     }
